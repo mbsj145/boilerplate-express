33ea4eab843e18d690a79c6f4a8561c653f7d5e6
mbsj145 <mbsj145@gmail.com>
2025-12-14 20:53:42 +0500

    asd

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 let express = require('express');
+let bodyParser = require('body-parser');  // 👈 require body-parser
 let app = express();
 
 console.log("Hello World");
@@ -10,6 +11,9 @@ app.use(function (req, res, next) {
   console.log(req.method + " " + req.path + " - " + req.ip);
   next();
 });
+
+// Body-parser middleware for URL-encoded data
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Serve static files
 app.use('/public', express.static(__dirname + '/public'));
@@ -32,6 +36,14 @@ app.get('/:word/echo', function (req, res) {
 app.get('/name', function (req, res) {
   const firstName = req.query.first;
   const lastName = req.query.last;
+
+  res.json({ name: `${firstName} ${lastName}` });
+});
+
+// POST /name endpoint
+app.post('/name', function (req, res) {
+  const firstName = req.body.first;
+  const lastName = req.body.last;
 
   res.json({ name: `${firstName} ${lastName}` });
 });
