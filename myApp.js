e0db600f23d40bbaefe3a98ac2bf2441cdcce9d7
mbsj145 <mbsj145@gmail.com>
2025-12-14 20:19:43 +0500

    a

--- myApp.js
+++ myApp.js
@@ -3,6 +3,9 @@ let app = express();
 
 console.log("Hello World");
 
+// 👇 Serve static assets
+app.use('/public', express.static(__dirname + '/public'));
+
 // Serve HTML file
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
