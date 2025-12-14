2a4a64aca83f292c93845e88af260888bf12de21
mbsj145 <mbsj145@gmail.com>
2025-12-14 20:15:53 +0500

    aaa

--- myApp.js
+++ myApp.js
@@ -1,39 +1,11 @@
 let express = require('express');
 let app = express();
 
-
-
-
-
-
-
 console.log("Hello World");
 
+// 👇 Add this code
+app.get('/', function (req, res) {
+  res.send('Hello Express');
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
