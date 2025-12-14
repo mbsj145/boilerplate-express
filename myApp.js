b4fdafc602dc0dd65007488e3a345bfcf62fab35
mbsj145 <mbsj145@gmail.com>
2025-12-14 20:44:25 +0500

    Update JSON API to conditionally format messageasd
    Refactor JSON API to use environment variable for message style.

--- myApp.js
+++ myApp.js
@@ -1,19 +1,27 @@
+require('dotenv').config();   // 👈 MUST be at the top
+
 let express = require('express');
 let app = express();
 
 console.log("Hello World");
 
-// Serve static assets
+// Static files
 app.use('/public', express.static(__dirname + '/public'));
 
-// Serve HTML file
+// Serve HTML
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// 👇 Serve JSON
+// 👇 JSON API with env variable
 app.get('/json', function (req, res) {
-  res.json({ message: "Hello json" });
+  let message = "Hello json";
+
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    message = message.toUpperCase();
+  }
+
+  res.json({ message: message });
 });
 
 module.exports = app;
